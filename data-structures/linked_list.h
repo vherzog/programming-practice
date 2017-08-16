bbd7dfed0e789192bfef3fe19c9e7b7bfcb317fb
v.herzog92 <v.herzog92@gmail.com>
2017-08-16 14:13:59 -0400

    add linked list header

---
+++ data-structures/linked_list.h
@@ -0,0 +1,31 @@
+/* linked_list.h */
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+
+class Node {
+  int data;
+  Node* next;
+  Node(int value);
+}
+
+class LinkedList {
+  LinkedList();
+  LinkedList(int value);
+  ~LinkedList();
+  int size();
+  bool empty();
+  int value_at(int index);
+  void push_front(int value);
+  int pop_front();
+  void push_back(int value);
+  int pop_back();
+  int front();
+  int back();
+  void insert(int index, int value);
+  void erase(int index) 
+  void print_list() 
+  int nth_from_end(int n) 
+  void reverse() 
+  void remove_value(int value)
+}
